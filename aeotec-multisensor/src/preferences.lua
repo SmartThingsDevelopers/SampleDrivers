cb46312fa14224f3b68ad6a71aba7d200f526655
nayelyzarazua-bluetrail <nayelyz@bluetrailsoft.com>
2021-08-13 18:57:27 -0500

    Add PIR time in preferences

--- aeotec-multisensor/src/preferences.lua
+++ aeotec-multisensor/src/preferences.lua
@@ -1,5 +1,6 @@
 local preferences = {
-  motionSensorSensitivity = {parameter_number = 4, size = 1}
+  motionSensorSensitivity = {parameter_number = 4, size = 1},
+  motionClearTime = {parameter_number = 3, size = 2}
 }
 
 return preferences
